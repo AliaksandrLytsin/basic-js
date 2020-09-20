cc8710172c99eaef4181a067752f0049f14e4d04
Aliaksandr Lytsin <alexgooner13@gmail.com>
2020-09-21 01:44:22 +0300

    feat: complete dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!(Array.isArray(members))) return false;
+  let filtered = members.filter(member => typeof(member) === 'string');
+  let noWhitespaceUpper = filtered.map(word => word.trim().toUpperCase());
+  let sorted = noWhitespaceUpper.sort();
+  let result = '';
+  sorted.forEach(word => result += word[0]);
+  return result;
 };
